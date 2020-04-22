67c7cac0beedcb032582e1bc2644a13754f7a3a7
armarm514 <21500514@handong.edu>
2020-04-22 14:53:07 +0900

    3.3 Finshing User Model

--- users/models.py
+++ users/models.py
@@ -3,20 +3,21 @@ from django.db import models
 
 # Create your models here.
 class User(AbstractUser):
-    """ Custom User Model"""
-
-    avatar = models.ImageField(null=True)
+    """" Custom User Model"""
 
     GENDER_MALE = 0
     GENDER_FEMALE = 1
     GENDER_CHOICES = [(GENDER_MALE, "Male"), (GENDER_FEMALE, "Female")]
 
-    gender1 = models.CharField(
-        choices=GENDER_CHOICES, max_length=10, null=True, blank=True
-    )
-    gender = models.IntegerField(choices=GENDER_CHOICES, default=0, blank=True)
-    # gender = models.BooleanField(choices=GENDER_CHOICES, default=0)
-    bio = models.TextField(default="", blank=True)  # null=True ignore that empty~
+    LANGUAGE_ENGLISH = "en"
+    LANGUAGE_KOREAN = "kr"
+    LANGUAGE_CHOICES = [(LANGUAGE_KOREAN, "korean"), (LANGUAGE_ENGLISH, "english")]
+
+    CURRENCY_USD = "usb"
+    CURRENCY_KRW = "krw"
+    CURRENCY_CHICES = [(CURRENCY_KRW, "KRW"), (CURRENCY_USD, "USB")]
+
+    avatar = models.ImageField(null=True, blank=True)
     # 여기서 만든 것을 장고가 form 으로 만들어줘 그리고 장고는 database에 Migration과 함께 이 form에 필요한 정보를 요청할 거야
     """admin.py에다가 
     @admin.register(models.User)
@@ -24,3 +25,25 @@ class User(AbstractUser):
         pass
     """
     # # in django document you can search what tpye of model fields you will use!
+
+    gender = models.IntegerField(choices=GENDER_CHOICES, default=0, blank=True)
+    bio = models.TextField(default="", blank=True)  # null=True ignore that empty~
+    birthdate = models.DateField(null=True, blank=True)
+
+    language = models.CharField(
+        choices=LANGUAGE_CHOICES,
+        max_length=2,
+        null=True,
+        blank=True,
+        default=LANGUAGE_KOREAN,
+    )
+
+    currency = models.CharField(
+        choices=CURRENCY_CHICES,
+        max_length=3,
+        null=True,
+        blank=True,
+        default=CURRENCY_KRW,
+    )
+
+    superhost = models.BooleanField(default=False)
