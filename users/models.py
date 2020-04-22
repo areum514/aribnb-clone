f913287de0df13b879c2c23c12d17598e0096eac
armarm514 <21500514@handong.edu>
2020-04-22 13:33:59 +0900

    3.1 introduction to the User Model

--- users/models.py
+++ users/models.py
@@ -4,3 +4,11 @@ from django.db import models
 # Create your models here.
 class User(AbstractUser):
     pass
+    bio = models.TextField(default="")
+    # 여기서 만든 것을 장고가 form 으로 만들어줘 그리고 장고는 database에 Migration과 함께 이 form에 필요한 정보를 요청할 거야
+    """admin.py에다가 
+    @admin.register(models.User)
+    class CustomUserAdmin(admin.ModelAdmin):
+        pass
+    """
+    # # in django document you can search what tpye of model fields you will use!
