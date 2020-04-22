5ab3637fad225b36782a76a80ac34fdfb0348677
armarm514 <21500514@handong.edu>
2020-04-22 14:31:09 +0900

    3.2 FIrst Model Fields

--- users/models.py
+++ users/models.py
@@ -3,8 +3,20 @@ from django.db import models
 
 # Create your models here.
 class User(AbstractUser):
-    pass
-    bio = models.TextField(default="")
+    """ Custom User Model"""
+
+    avatar = models.ImageField(null=True)
+
+    GENDER_MALE = 0
+    GENDER_FEMALE = 1
+    GENDER_CHOICES = [(GENDER_MALE, "Male"), (GENDER_FEMALE, "Female")]
+
+    gender1 = models.CharField(
+        choices=GENDER_CHOICES, max_length=10, null=True, blank=True
+    )
+    gender = models.IntegerField(choices=GENDER_CHOICES, default=0, blank=True)
+    # gender = models.BooleanField(choices=GENDER_CHOICES, default=0)
+    bio = models.TextField(default="", blank=True)  # null=True ignore that empty~
     # 여기서 만든 것을 장고가 form 으로 만들어줘 그리고 장고는 database에 Migration과 함께 이 form에 필요한 정보를 요청할 거야
     """admin.py에다가 
     @admin.register(models.User)
