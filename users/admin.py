a79420522c04e55f6f2ed0094d9241d0b4f18f2d
armarm514 <21500514@handong.edu>
2020-04-23 09:21:25 +0900

    3.4Falling in Love with Admin Panel

--- users/admin.py
+++ users/admin.py
@@ -4,4 +4,7 @@ from . import models
 # Register your models here.
 @admin.register(models.User)
 class CustomUserAdmin(admin.ModelAdmin):
-    pass
+    """Custom User Admin"""
+
+    list_display = ("username", "gender", "superhost")
+    list_filter = ("superhost", "gender")
