1f5e61acffea845c2466f7d2ca70f8a38df68fd8
armarm514 <21500514@handong.edu>
2020-04-24 13:55:14 +0900

    5.3 Converstation and Messages Models

--- lists/models.py
+++ lists/models.py
@@ -10,3 +10,6 @@ class List(core_models.TimeStampedMode):
     name = models.CharField(max_length=80)
     user = models.ForeignKey("users.User", on_delete=models.CASCADE)
     rooms = models.ManyToManyField("rooms.Room", blank=True)
+
+    def __str__(self):
+        return self.name
