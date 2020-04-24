4072b7f788cf0e81652bf626916a5308ba9c7625
armarm514 <21500514@handong.edu>
2020-04-24 09:53:41 +0900

    4.4. on_delete,Amenty, Facility, JouseRule Models

--- rooms/admin.py
+++ rooms/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from . import models
 
 # Register your models here.
-@admin.register(models.RoomTyte)
+@admin.register(models.RoomTyte, models.Facility, models.HouseRule, models.Amenity)
 class ItemAdim(admin.ModelAdmin):
     pass
 
--- rooms/models.py
+++ rooms/models.py
@@ -17,6 +17,26 @@ class AbstractItem(core_models.TimeStampedMode):
 
 
 class RoomTyte(AbstractItem):
+    """RoomType Model Definition"""
+
+    pass
+
+
+class Amenity(AbstractItem):
+    """Amenity Model Definition"""
+
+    pass
+
+
+class Facility(AbstractItem):
+    """Facility Model Definition"""
+
+    pass
+
+
+class HouseRule(AbstractItem):
+    """HouseRule Model Definition"""
+
     pass
 
 
@@ -37,8 +57,13 @@ class Room(core_models.TimeStampedMode):
     check_in = models.TimeField()
     check_out = models.TimeField()
     instant_book = models.BooleanField(default=False)
+    # CASCADE 폭포수 여기서 USER를 지우면 그거와 관련된 ROOM을 을 다 지우겠다 폭포수 효과
+    # on_delete 는 1대 다만 사용..!
     host = models.ForeignKey(user_models.User, on_delete=models.CASCADE)
-    room_type = models.ManyToManyField(RoomTyte, blank=True)
+    room_type = models.ForeignKey(RoomTyte, on_delete=models.SET_NULL, null=True)
+    amenity = models.ManyToManyField(Amenity)
+    facility = models.ManyToManyField(Facility)
+    house_rules = models.ManyToManyField(HouseRule)
 
     def __str__(self):
         return self.name
