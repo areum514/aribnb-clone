8d0668d80ca6f2c0345e5a97713187cf13a33941
armarm514 <21500514@handong.edu>
2020-05-05 15:55:03 +0900

    15.1 signup form part two

--- users/forms.py
+++ users/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from . import models
 
 class LoginForm(forms.Form):
-    
+
     email=forms.EmailField()
     password=forms.CharField(widget=forms.PasswordInput)
     
@@ -18,4 +18,39 @@ class LoginForm(forms.Form):
                 self.add_error("password",forms.ValidationError("Password is wrong"))
         except models.User.DoesNotExist:
             raise forms.ValidationError("User does not exist")
+
+
+class SignUpForm(forms.Form):
+    first_name=forms.CharField(max_length=50)
+    last_name=forms.CharField(max_length=50)
+    email=forms.EmailField()
+    password=forms.CharField(widget=forms.PasswordInput)
+    password1=forms.CharField(widget=forms.PasswordInput,label="confirm password")
     
+    def clean_email(self):
+        email = self.cleaned_data.get("email")
+        
+        try:    
+            models.User.objects.get(email=email)
+            raise forms.ValidationError("User already exists with that email")
+        except models.User.DoesNotExist:
+            return email
+
+    def clean_password1(self):
+        password = self.cleaned_data.get("password")
+        password1 = self.cleaned_data.get("password1")
+        if password != password1:
+            raise forms.ValidationError("password confirmation does not match")
+        else:
+            return password
+
+    def save(self):
+        first_name = self.cleaned_data.get("first_name")
+        last_name = self.cleaned_data.get("last_name")
+        email = self.cleaned_data.get("email")
+        password = self.cleaned_data.get("password")
+        
+        user=models.User.objects.create_user(email,email=email,password=password)
+        user.first_name=first_name
+        user.last_name=last_name
+        user.save()
